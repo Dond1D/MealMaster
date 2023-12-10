17963acb92611b4899ae05ced5ccc90c752e6657
Donjet <109423101+Dond1D@users.noreply.github.com>
2023-12-10 22:09:38 +0100

    Delete EmployeRepo.java

--- EmployeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeRepo extends JpaRepository<Employee,Long> {
-}
