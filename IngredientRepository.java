a607d08335b3faf7073a7ab92de1270f18e30156
Donjet <109423101+Dond1D@users.noreply.github.com>
2023-12-10 22:10:18 +0100

    Delete IngredientRepository.java

--- IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient,Integer> {
-}
