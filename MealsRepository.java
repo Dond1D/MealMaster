d6e3a2edd06143cccb0b3f7fb01270673a7f2fdb
Donjet <109423101+Dond1D@users.noreply.github.com>
2023-12-10 22:11:15 +0100

    Delete MealsRepository.java

--- MealsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Meal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MealsRepository extends JpaRepository<Meal,Integer> {
-}
