ef1644d3adba0acaba3d026cec88c415e1a507d1
Donjet <109423101+Dond1D@users.noreply.github.com>
2023-12-10 22:09:31 +0100

    Delete DietRepository.java

--- DietRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Diet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DietRepository extends JpaRepository<Diet,Integer> {
-}
